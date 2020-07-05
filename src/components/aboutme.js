c0b1976200fadcbd9bf40b110945edc6c7e19005
agradilenko <44676263+agradilenko@users.noreply.github.com>
2020-07-05 17:51:50 +0300

    Initial commit

---
+++ src/components/aboutme.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import {Cell, Grid} from "react-mdl";
+
+class About extends Component {
+  render() {
+    return(
+      <div>
+        <Grid>
+        <Cell col={4}>
+          <img
+            src="https://sun9-36.userapi.com/c638219/v638219870/4fa05/LF6dRL0BAzk.jpg"
+            alt="avatar"
+            className="avatar-img"
+          />
+
+          <h2>Gradilenko Artem</h2>
+          <h4 style={{color: 'grey'}}>Student, Programmer</h4>
+          <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+          <h5>Address</h5>
+          <p>Russia, Nizhny Novgorod</p>
+          <h5>Phone</h5>
+          <p>89875308414</p>
+          <h5>Email</h5>
+          <p>artem.gradilenko@gmail.com</p>
+          <h5>Web</h5>
+          <a href="https://github.com/agradilenko" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-github-square" aria-hidden="true" />
+          </a>
+          <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+        </Cell>
+        <Cell className="aboutme-right-col" col={8}>
+          <h2>About me</h2>
+          <article className="aboutme-post">
+            <h5>Intro</h5>
+            <p>My name is Artem. I am studying in the third course at Nizhny Novgorod State Technical University.</p>
+            <p>I have been interested in web technologies since I graduated my school</p>
+          </article>
+          <hr style={{borderTop: '3px solid #e22947'}} />
+          <article className="aboutme-post">
+            <h5>I like</h5>
+              <ul>
+                <li>Space</li>
+                <li>Books</li>
+                <li>Summer</li>
+                <li>Sci-fi films</li>
+                <li>Rock music</li>
+              </ul>
+          </article>
+          <hr style={{borderTop: '3px solid #e22947'}} />
+          <article className="aboutme-post">
+            <h5>I dream of</h5>
+            <ul>
+              <li>doing better</li>
+              <li>always finding inspiration</li>
+              <li>find a wonderful job</li>
+              <li>take a trip around the world</li>
+              <li>visit Juventus football match</li>
+            </ul>
+          </article>
+        </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default About;
---
+++ src/components/project.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardTitle,
+  CardText,
+  CardActions,
+  Button,
+  Cell,
+} from "react-mdl";
+import {Link} from "@material-ui/core";
+
+class Project extends Component {
+  render() {
+    return (
+        <Cell col={4}>
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "black",
+                height: "450px",
+                padding: "10px",
+                background: `url(${this.props.backgroundUrl}) center / cover`,
+              }}
+            >
+              {this.props.projectName}
+            </CardTitle>
+            <CardText>{this.props.projectDescription}</CardText>
+            <CardActions border>
+              <Link href = {this.props.gitUrl}>
+              <Button colored>GitHub</Button>
+              </Link>
+              <Link href = {this.props.demoUrl}>
+              <Button colored>Live Demo</Button>
+              </Link>
+            </CardActions>
+          </Card>
+        </Cell>
+    );
+  }
+}
+
+export default Project;
